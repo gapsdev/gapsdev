ff912b9240f5f09be8bb2173be32eb95bc58f78f
gapsdev <78837414+gapsdev@users.noreply.github.com>
2026-09-22 23:57:00 +0800

    Configure static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: { remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }] },
+  output: "export",
+
+  poweredByHeader: false,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
